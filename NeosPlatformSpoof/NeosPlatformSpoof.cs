089b5ad5a996f4a5513906f2094d750af628a9ce
Michael Ripley <zkxs00@gmail.com>
2022-01-10 07:32:39 -0600

    Add configuration support

--- NeosPlatformSpoof/NeosPlatformSpoof.cs
+++ NeosPlatformSpoof/NeosPlatformSpoof.cs
@@ -2,6 +2,7 @@
 using HarmonyLib;
 using NeosModLoader;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace NeosPlatformSpoof
@@ -10,15 +11,26 @@ namespace NeosPlatformSpoof
     {
         public override string Name => "NeosPlatformSpoof";
         public override string Author => "runtime";
-        public override string Version => "1.0.0";
+        public override string Version => "1.1.0";
         public override string Link => "https://github.com/zkxs/NeosPlatformSpoof";
 
-        private static readonly Platform TARGET_PLATFORM = Platform.Android;
-
         private static FieldInfo _userInitializingEnabled;
+        private static ModConfiguration _config;
+
+        private static readonly ModConfigurationKey<bool> ENABLED = new ModConfigurationKey<bool>("enabled", "Enable platform spoofing", () => true);
+        private static readonly ModConfigurationKey<Platform> TARGET_PLATFORM = new ModConfigurationKey<Platform>("spoofed platform", "This will appear as your platform in new sessions you join.", () => Platform.Android);
+
+        public override ModConfigurationDefinition GetConfigurationDefinition()
+        {
+            HashSet<ModConfigurationKey> keys = new HashSet<ModConfigurationKey>();
+            keys.Add(ENABLED);
+            keys.Add(TARGET_PLATFORM);
+            return DefineConfiguration(new Version(1, 0, 0), keys);
+        }
 
         public override void OnEngineInit()
         {
+            _config = GetConfiguration();
             Harmony harmony = new Harmony("net.michaelripley.NeosPlatformSpoof");
 
             _userInitializingEnabled = AccessTools.DeclaredField(typeof(User), "InitializingEnabled");
@@ -37,13 +49,14 @@ namespace NeosPlatformSpoof
         {
             private static void Prefix(ref SyncMessage message)
             {
-                if (message is ControlMessage controlMessage && controlMessage.ControlMessageType == ControlMessage.Message.JoinRequest)
+                if (_config.GetValue(ENABLED) && message is ControlMessage controlMessage && controlMessage.ControlMessageType == ControlMessage.Message.JoinRequest)
                 {
                     Platform oldPlatform = Platform.Other;
                     if (controlMessage.Data.TryExtract("Platform", ref oldPlatform))
                     {
-                        controlMessage.Data.AddOrUpdate("Platform", TARGET_PLATFORM);
-                        Msg($"spoofed join Platform from {oldPlatform} to {TARGET_PLATFORM}");
+                        Platform newPlatform = _config.GetValue(TARGET_PLATFORM);
+                        controlMessage.Data.AddOrUpdate("Platform", newPlatform);
+                        Msg($"spoofed join Platform from {oldPlatform} to {newPlatform}");
                     }
                 }
             }
@@ -54,12 +67,15 @@ namespace NeosPlatformSpoof
         {
             private static void Postfix(ref User __result)
             {
-                Platform oldPlatform = __result.Platform;
-                bool oldInitializingEnabled = (bool) _userInitializingEnabled.GetValue(__result);
-                _userInitializingEnabled.SetValue(__result, true);
-                __result.Platform = TARGET_PLATFORM;
-                _userInitializingEnabled.SetValue(__result, oldInitializingEnabled);
-                Msg($"spoofed host platform from {oldPlatform} to {__result.Platform}");
+                if (_config.GetValue(ENABLED))
+                {
+                    Platform oldPlatform = __result.Platform;
+                    bool oldInitializingEnabled = (bool)_userInitializingEnabled.GetValue(__result);
+                    _userInitializingEnabled.SetValue(__result, true);
+                    __result.Platform = _config.GetValue(TARGET_PLATFORM);
+                    _userInitializingEnabled.SetValue(__result, oldInitializingEnabled);
+                    Msg($"spoofed host platform from {oldPlatform} to {__result.Platform}");
+                }
             }
         }
     }
